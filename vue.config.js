910b7bafcd076fd913fb46a62c9ff0ab5a79737f
K-Philleani <964202623@qq.com>
2021-08-13 22:59:11 +0800

    feat(main): 配置vue.congif.js文件

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  // 配置方式一: cli提供的配置方式
+  outputDir: './dist',
+  // 配置方式二: 和webpack属性完全一致,最后会进行合并
+  // configureWebpack: {
+  //   alias: {
+  //     components: '@/components'
+  //   }
+  // }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     components: '@/components'
+  //   }
+  // }
+  // 配置方式三
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('components', '@/components')
+  }
+}
