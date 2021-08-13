07df5d465dad541ced0b3ce963be8190a4ad6c1f
K-Philleani <964202623@qq.com>
2021-08-14 01:08:26 +0800

    feat(main): 引入vuex

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router'
+import Store from './store'
 
 const app = createApp(App)
 app.use(Router)
+app.use(Store)
 
 app.mount('#app')
