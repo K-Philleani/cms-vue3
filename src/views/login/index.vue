13fdc7f4551f81a70613ac4f79f41c5fd7a54efb
K-Philleani <964202623@qq.com>
2021-08-14 00:47:43 +0800

    feat(main): 引入vue-router

---
+++ src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>2</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'index',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
