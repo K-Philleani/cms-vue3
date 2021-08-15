4c62162ed1f95bb605cb726f6301d40ae27471df
K-Philleani <964202623@qq.com>
2021-08-15 22:41:37 +0800

    feat(process.env): 区分环境变量

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
+import type { AxiosResponse } from 'axios'
 
 // axios基本配置项
 axios.defaults.baseURL = 'http://123.207.32.32:8000'
@@ -7,3 +8,6 @@ axios.defaults.timeout = 5000
 axios.get('/home/multidata').then((res: AxiosResponse) => {
   console.log(res.data)
 })
+
+console.log(process.env.NODE_ENV)
+console.log(process.env.VUE_APP_BASE_URL)
